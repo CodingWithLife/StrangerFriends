96019efa2393c21e480d249d115491a569dcaeaf
Erickdegnte <erickdegante3003@gmail.com>
2018-10-22 15:58:42 -0500

    the every person

--- react-client/src/components/archetypes/everyperson.jsx
+++ react-client/src/components/archetypes/everyperson.jsx
@@ -4,16 +4,15 @@ import React from 'react';
 
 class Everyperson extends React.Component {
     render () {
-    return (
-      <div>
-      <h1>This is outlaw archety...
-      if you want another one ..
-      you have to create a file inside the
-      archetypes folder and assigned it in the
-       index.jsx as the component to its
-       correspondent route</h1>
-      </div>)
-  }
+      return (
+        <div className='the everyperson'>
+        <h1>The Everyperson.
+          Morality, virtue, and equality are important–and when you are an Everyperson, perhaps they are appreciated more than anything else.
+          Among the twelve archetypes, there are none more “centered” than he.
+          The Everyperson is not just centered in heart and spirituality and education, but also in the “wheel” of archetypes.
+          The Everyperson can easily turn into any of the other Soul or Self types.</h1>
+        </div>)
+      }
 }
 
 export default Everyperson;
