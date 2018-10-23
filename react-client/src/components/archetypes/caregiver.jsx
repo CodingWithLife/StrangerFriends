af8fc7f51782570d9e97de38b3a3d7a693203873
Silvina Delgado <silvina.ruiz@gmail.com>
2018-10-23 17:12:29 -0500

    css profiles

--- react-client/src/components/archetypes/caregiver.jsx
+++ react-client/src/components/archetypes/caregiver.jsx
@@ -6,15 +6,33 @@ class Caregiver extends React.Component {
     render () {
       return (
         <div className='caregiver'>
-        <h1>The Caregiver
-          Those who identify with the caregiver archetypes are full of empathy and compassion. Unfortunately, others can exploit their good nature for their own ends. Caregivers must pay attention to looking after themselves and learning to say no to others’ demands sometimes.
+        <h3 className="title1">The Caregiver</h3>
+          <p>Those who identify with the caregiver archetypes are full of empathy and compassion.
+          Unfortunately, others can exploit their good nature for their own ends.
+          Caregivers must pay attention to looking after themselves and learning to say no to others’ demands sometimes.
 
-          <li>Goal: to help others</li>
-          <li>Fear: being considered selfish</li>
-          <li>Weakness: being exploited by others and feeling put upon</li>
-          <li>Talent: compassion and generosity</li>
 
-        </h1>
+            <ul><strong className='bold1'>Goal:</strong> to help others</ul>
+            <ul>Fear: being considered selfish</ul>
+            <ul>Weakness: being exploited by others and feeling put upon</ul>
+            <ul>Talent: compassion and generosity</ul>
+          </p>
+
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+
+
+
         </div>)
        }
       }
--- react-client/src/components/archetypes/explorer.jsx
+++ react-client/src/components/archetypes/explorer.jsx
@@ -6,14 +6,13 @@ class Explorer extends React.Component {
     render () {
       return (
         <div className='explorer'>
-          <h1>The EXPLORER break free
-            Freedom is the top priority for an explorer.
-            Where other brands might try to help you build a home,
-            these brands want to get you out of it.
-            Subaru is the classic fit for this archetype.
-            The company doesn’t sell cars based on luxury or comfort – instead,
-            freedom is the focus. Blizzard? No problem. Subaru lets you decide where you’re going,
-            no matter the circumstance –You’re free.\
+          <h1>TThe Explorer
+            The explorer is never happy unless experiencing new things. They may enjoy visiting different countries or they may be happy learning about new ideas and philosophies. However, they find it hard to settle down at one job or relationship for too long, unless the job or relationship lets them retain their freedom to explore.
+
+            Goal: to experience as much of life as possible in one lifetime
+            Fear: getting trapped or being forced to conform
+            Weakness: aimless wandering and inability to stick at things
+            Talent: being true to their own desires and a sense of wonder
             </h1>
         </div>)
       }
