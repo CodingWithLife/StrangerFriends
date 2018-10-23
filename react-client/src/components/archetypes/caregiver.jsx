1df2f4eef38b62c2977e3dae42b3da280e17da23
Silvina Delgado <silvina.ruiz@gmail.com>
2018-10-23 14:19:09 -0500

    changed text on profiles

--- react-client/src/components/archetypes/caregiver.jsx
+++ react-client/src/components/archetypes/caregiver.jsx
@@ -6,11 +6,14 @@ class Caregiver extends React.Component {
     render () {
       return (
         <div className='caregiver'>
-        <h1>The CAREGIVER nurtures you
-          The caregiver is benevolent and just wants to be there for you.
-          Caregiver brands build trust.
-          It’s rare to see a caregiver brand run an ad that takes a shot at their competition.
-          They are the opposite of confrontational.
+        <h1>The Caregiver
+          Those who identify with the caregiver archetypes are full of empathy and compassion. Unfortunately, others can exploit their good nature for their own ends. Caregivers must pay attention to looking after themselves and learning to say no to others’ demands sometimes.
+
+          <li>Goal: to help others</li>
+          <li>Fear: being considered selfish</li>
+          <li>Weakness: being exploited by others and feeling put upon</li>
+          <li>Talent: compassion and generosity</li>
+
         </h1>
         </div>)
        }
