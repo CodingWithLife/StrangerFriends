4c07f82c07338a94da171c725960ef4289ab97a8
Silvina Delgado <silvina.ruiz@gmail.com>
2018-10-23 19:39:49 -0500

    Css profiles for caregiver

--- react-client/src/components/archetypes/creator.jsx
+++ react-client/src/components/archetypes/creator.jsx
@@ -6,13 +6,38 @@ class Creator extends React.Component {
     render () {
       return (
         <div className='the creator'>
-        <h1>The Creator
-          The creator is born to bring something into being that does not yet exist. They hate to be passive consumers of anything, much preferring to make their own entertainment. Creators are often artists or musicians though they can be found in almost any area of work.
+          <h3 className='title1'>The Creator</h3>
+            <p>The creator is born to bring something into being that does not yet exist.
+              They hate to be passive consumers of anything, much preferring to make their own entertainment.
+              Creators are often artists or musicians though they can be found in almost any area of work.
+            </p>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
 
-          Goal: to create things of enduring value
-          Fear: failing to create anything great
-          Weakness: perfectionism and creative blocks caused by fear of not being exceptional
-          Talent: creativity and imagination</h1>
+            <p>
+              <ul><strong className='bold1'>Goal:</strong> to create things of enduring value</ul>
+              <ul><strong className='bold1'>Fear:</strong> failing to create anything great</ul>
+              <ul><strong className='bold1'>Weakness:</strong> perfectionism and creative blocks caused by fear of not being exceptional</ul>
+              <ul><strong className='bold1'>Talent:</strong> creativity and imagination</ul>
+            </p>
+
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+
         </div>)
       }
     }
